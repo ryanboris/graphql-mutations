38aa75595ad707e905ddff88d5c85172b3ba35d3
ryanboris <ryan.boris@gmail.com>
2018-12-23 01:48:36 -0500

    completes the refactoring of the file structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import { GraphQLServer } from 'graphql-yoga'
-import { resolvers } from './resolvers'
 import db from './db'
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
+import Post from './resolvers/Post'
+import User from './resolvers/User'
+import Comment from './resolvers/Comment'
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers,
+    resolvers: {
+        Query,
+        Mutation,
+        Post,
+        User,
+        Comment
+    },
     context: {
         db
     }

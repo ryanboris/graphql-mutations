1ee5cf86a272b51b25124b101a86a1ec9edb6aa6
ryanboris <ryan.boris@gmail.com>
2018-12-23 15:42:42 -0500

    adds posts subscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -72,7 +72,7 @@ const Mutation = {
 
         return user
     },
-    createPost(parent, args, { db }, info) {
+    createPost(parent, args, { db, pubsub }, info) {
         const userExists = db.users.some(user => user.id === args.data.author)
         if (!userExists) {
             throw new Error('User not found.')
@@ -84,6 +84,11 @@ const Mutation = {
         }
 
         db.posts.push(post)
+
+        if (args.data.published) {
+            pubsub.publish('post', { post })
+        }
+
         return post
     },
 
